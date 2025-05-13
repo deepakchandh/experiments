663662906ca819f287a79dd0d17c0dccb9e05558
Deepak Chandh <deepak.chandh@tesco.com>
2025-05-13 17:31:57 +0530

    unidirectional crud

--- deptservice/src/main/java/com/java/deptservice/entity/Controller.java
+++ deptservice/src/main/java/com/java/deptservice/entity/Controller.java
@@ -4,19 +4,24 @@ import com.java.deptservice.entity.entity.Address;
 import com.java.deptservice.entity.entity.Order;
 import com.java.deptservice.entity.repository.OrderRepo;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/join")
 public class Controller {
 
     private OrderRepo orderRepo;
 
-
-    @PostMapping("save")
+    @PostMapping
     void saveData(){
         Order orderyy = new Order();
         orderyy.setOrderTrackingNumber("100ac");
@@ -30,9 +35,17 @@ public class Controller {
         address.setStreet("whale");
         address.setCountry("India");
         address.setZipCode("651101");
-
         orderyy.setBillingAddress(address);
         orderRepo.save(orderyy);
+    }
 
+    @DeleteMapping("/{id}")
+    void deleteData(@PathVariable("id") Long id){
+        orderRepo.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    Optional<Order> getData(@PathVariable("id") Long id){
+        return orderRepo.findById(id);
     }
 }
